53dbd65757340ed211d397de7d2db13faa6fe3ad
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-05-14 17:53:20 +0200

    feat(sidebar): get all categories in an array

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
+"use client"
 import React from 'react';
 import Header from '../src/components/Header/Header';
+import { REACT_CARDS } from '@/src/react-cards';
 
 const page = () => {
+
+ const filters = [...new Set(REACT_CARDS.map(card => card.category))]
+console.log(filters)
+
+
   return (
     <main className='mx-auto max-w-4xl flex flex-col px-4'>
      <Header />
